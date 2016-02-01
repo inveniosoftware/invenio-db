5271fdd50664ea2e73aefa2eaa84a233c536a17a
Sami Hiltunen <sami.mikael.hiltunen@cern.ch>
2016-02-01 14:53:45 +0100

    installation: pydocstyle>=1.0.0
    * Replaces pep257 with pydocstyle.
    
    Signed-off-by: Sami Hiltunen <sami.mikael.hiltunen@cern.ch>

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of Invenio.
-# Copyright (C) 2015 CERN.
+# Copyright (C) 2015, 2016 CERN.
 #
 # Invenio is free software; you can redistribute it
 # and/or modify it under the terms of the GNU General Public License as
@@ -22,7 +22,7 @@
 # waive the privileges and immunities granted to it by virtue of its status
 # as an Intergovernmental Organization or submit itself to any jurisdiction.
 
-pep257 invenio_db && \
+pydocstyle invenio_db && \
 isort -rc -c -df **/*.py && \
 check-manifest --ignore ".travis-*" && \
 sphinx-build -qnNW docs docs/_build/html && \
