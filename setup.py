01f9b62b47229a0eaa87d55fd755d5f17c43712b
vladbmihaescu <vladbmihaescu@protonmail.ch>
2019-07-22 14:30:08 +0200

    travis: add postgresql 10 test

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ tests_require = [
 
 extras_require = {
     'docs': [
-        'Sphinx>=1.5.1',
+        'Sphinx>=1.8.0',
     ],
     'mysql': [
         'pymysql>=0.6.7',
@@ -56,7 +56,7 @@ install_requires = [
     'Flask-Alembic>=2.0.1',
     'Flask-SQLAlchemy>=2.1',
     'SQLAlchemy>=1.0',
-    'SQLAlchemy-Utils>=0.31.0',
+    'SQLAlchemy-Utils>=0.33.1',
 ]
 
 packages = find_packages()
