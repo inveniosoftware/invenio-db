2f59c21e4fd4ada20e98b9b733d9a599a1c3b5f9
Rokas Maciulaitis <rokas.maciulaitis@cern.ch>
2019-11-18 14:07:13 +0100

    setup: bumps SQLAlchemy version

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ install_requires = [
     'Flask>=0.11.1',
     'Flask-Alembic>=2.0.1',
     'Flask-SQLAlchemy>=2.1',
-    'SQLAlchemy>=1.0',
+    'SQLAlchemy>=1.1.0',
     'SQLAlchemy-Utils>=0.33.1',
 ]
 
