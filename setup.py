07793cc126f36bd911266cc2d79fc5e2ba192e53
Guillaume Viger <fenekku@fenekku.com>
2021-03-18 15:40:12 -0500

    dependencies: cap flask-sqlalchemy < 2.5
    Otherwise experience this breaking change:
    
    self = <flask_sqlalchemy._EngineConnector object at 0x7f80aafb65c0>, sa_url = postgresql+psycopg2://invenio:***@localhost:5432/invenio, echo = False
    
        def get_options(self, sa_url, echo):
            options = {}
    
            options = self._sa.apply_pool_defaults(self._app, options)
    >       sa_url, options = self._sa.apply_driver_hacks(self._app, sa_url, options)
    E       TypeError: 'NoneType' object is not iterable
    
    .venv/lib/python3.6/site-packages/flask_sqlalchemy/__init__.py:608: TypeError

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 #
 # This file is part of Invenio.
 # Copyright (C) 2015-2020 CERN.
+# Copyright (C) 2021 Northwestern University.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -44,7 +45,7 @@ for reqs in extras_require.values():
 install_requires = [
     'invenio-base>=1.2.3',
     'Flask-Alembic>=2.0.1',
-    'Flask-SQLAlchemy>=2.1',
+    'Flask-SQLAlchemy>=2.1,<2.5.0',
     'SQLAlchemy>=1.2.18,<1.4.0',
     'SQLAlchemy-Utils>=0.33.1,<0.36',
 ]
