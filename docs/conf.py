6e8950b29ef0036e53595d1a4ed24b00aff0e197
Christoph Ladurner <christoph.ladurner@tugraz.at>
2024-12-16 22:05:37 +0100

    fix: docs reference target not found

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,7 @@
 #
 # This file is part of Invenio.
 # Copyright (C) 2015-2018 CERN.
-# Copyright (C) 2022 Graz University of Technology.
+# Copyright (C) 2022-2024 Graz University of Technology.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -316,3 +316,8 @@ intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
 
 # Autodoc configuraton.
 autoclass_content = "both"
+
+nitpick_ignore = [
+    ("py:class", "TypeDecorator"),
+    ("py:class", "sqlalchemy.sql.sqltypes.DateTime"),
+]
