916b95d1bdeca7e196af5f7ba021e923ff519fc2
zzacharo <zacharias.zacharodimos@cern.ch>
2025-11-18 09:42:40 +0100

    release: v2.1.2

--- invenio_db/__init__.py
+++ invenio_db/__init__.py
@@ -92,7 +92,7 @@ version locations is assembled from ``invenio_db.alembic`` entry point group.
 from .ext import InvenioDB
 from .shared import db
 
-__version__ = "2.1.1"
+__version__ = "2.1.2"
 
 __all__ = (
     "__version__",
