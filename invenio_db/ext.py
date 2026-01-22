f9ef0f4c905797eb330a7cafaafb965b854d8d97
Christoph Ladurner <christoph.ladurner@tugraz.at>
2026-01-22 20:06:50 +0100

    fix: str of datetime

--- invenio_db/ext.py
+++ invenio_db/ext.py
@@ -3,7 +3,7 @@
 # This file is part of Invenio.
 # Copyright (C) 2015-2018 CERN.
 # Copyright (C) 2022 RERO.
-# Copyright (C) 2022 Graz University of Technology.
+# Copyright (C) 2022-2026 Graz University of Technology.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
