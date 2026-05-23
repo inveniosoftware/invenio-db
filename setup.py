adcb43f3628469af9c0e6eba591671bf667e6e53
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-05-23 20:50:33 +0200

    chore(licenses): update license headers to use SPDX

--- setup.py
+++ setup.py
@@ -1,12 +1,7 @@
-# -*- coding: utf-8 -*-
-#
-# This file is part of Invenio.
-# Copyright (C) 2015-2022 CERN.
-# Copyright (C) 2021 Northwestern University.
-# Copyright (C) 2022 RERO.
-#
-# Invenio is free software; you can redistribute it and/or modify it
-# under the terms of the MIT License; see LICENSE file for more details.
+# SPDX-FileCopyrightText: 2015-2022 CERN.
+# SPDX-FileCopyrightText: 2021 Northwestern University.
+# SPDX-FileCopyrightText: 2022 RERO.
+# SPDX-License-Identifier: MIT
 
 """Database management for Invenio."""
 
